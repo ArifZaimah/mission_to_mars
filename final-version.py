bd77c7508ef916ad2a56d2456993e3f45df68bf1
ArifZaimah <ahmadarif.az@gmail.com>
2026-02-11 15:36:52 +0800

    completed version

---
+++ final-version.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+print("How much food do you to bring during your travel?")
+food = float(input("Ton(s) of food:"))
+print("How much water do you to bring during your travel?")
+water = float(input("Kilolitre(s) of water:"))
+
+def rocket():
+  distanceFromTop = 20
+  while distanceFromTop > 0:
+    print("\n         MARS")
+    print("\n" * distanceFromTop)
+    print("           /\ ")
+    print("           ||")
+    print("           ||")
+    print("          /||\ ")
+    print("\n")
+    print("arriving at destination in "+str(13.463*distanceFromTop)+" millions km")
+    print("food left is "+str(food/20*distanceFromTop)+" tons")
+    print("water left is "+str(water/20*distanceFromTop)+" kilolitres")
+    time.sleep(1)
+    os.system("cls")
+    distanceFromTop -= 1
+    
+rocket()
+print("YOU HAVE ARRIVE ON MARS")
+print("You have "+str(food/20)+" tons of food and "+str(water/20)+" kilolitres of water left to survive before finding a new resources")
+
+    
+    
+
+  
+  
+
